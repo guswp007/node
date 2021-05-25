e832d5b4bda07fabfa9f058c01439c485c51ef81
guswp007 <ghkdwp500@naver.com>
2021-05-25 11:10:13 +0900

    app.js 기본 셋팅

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const nunjucks = require('ninjucks');
+const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
@@ -13,30 +13,26 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set("view engin", "html");
 
-
 nunjucks.configure("views", {
 	express : app,
 	watch : true,
-)};
+});
 
 app.use(morgan('dev'));
 
-// 없는 페이지 처리 미들웨어
-
-//에러 처리 미들웨어
-
-app.use((err, req, res, next) => {
-	const error = new Error(`${req.method} ${req.url}은 없는 페이지 입니다`);
+// 없는 페이지 처리 미들웨어(라우터)
+app.use((req, res, next) => {
+	const error = new Error(`${req.method} ${req.url}은 없는 페이지 입니다.`);
 	error.status = 404;
-	next(error);
-
+	next(error); // 에러 처리 미들웨어로 전달 
 });
 
+// 에러 처리 미들웨어(라우터)
 app.use((err, req, res, next) => {
 	res.locals.error = err;
-
 	res.status(err.status || 500).render('error');
 });
 
-app.listern(app.get('port'), () => {
+app.listen(app.get('port'), () => {
 	console.log(app.get('port'), '번 포트에서 대기중');
+});
